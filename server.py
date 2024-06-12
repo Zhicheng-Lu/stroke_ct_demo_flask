d1e9d5b765ad703b2dc7b4c8bcbd2638cf3516b0
Zhicheng-Lu <andylu.zc@gmail.com>
2024-06-12 16:31:21 +1000

    v2.0

--- server.py
+++ server.py
@@ -23,14 +23,17 @@ def about():
 
 @app.route('/submit', methods=['POST'])
 def submit():
+	# Get time, create folder.
 	milli_sec = int(round(time.time() * 1000))
-	dt_string = datetime.now().strftime('%d/%m/%Y, %H:%M:%S')
+	in_time = datetime.now().strftime('%B %d, %Y - %H:%M:%S')
 	folder = f'static/output/{milli_sec}'
 	
+	# Extract uploaded file infomation
 	file = request.files['img']
 	filename = secure_filename(file.filename)
 	extension = filename.split('.')[-1]
 
+	# Extract file and save to local dir
 	if extension not in ['nii', 'dcm']:
 		return 'Please upload <b><i>.nii</i></b> or <b><i>.dcm</i></b> file!'
 
@@ -40,10 +43,15 @@ def submit():
 	nib_file = nib.load(f'{folder}/input.{extension}')
 	data = nib_file.get_fdata()
 	extract(folder, data)
+	extract1(folder, data)
 
-	generate_PDF(folder)
+	# Out time
+	out_time = datetime.now().strftime('%B %d, %Y - %H:%M:%S')
 
-	return render_template('report.html', time=dt_string, path=f'output/{milli_sec}/report.pdf')
+	generate_PDF(folder, in_time, out_time, num_files=data.shape[2])
+
+	return render_template('report.html', folder=f'output/{milli_sec}', in_time=in_time, out_time=out_time, num_files=data.shape[2])
+	# return ''
 
 
 def extract(folder, data):
@@ -52,6 +60,13 @@ def extract(folder, data):
 
 	for i,img in enumerate(data):
 		cv2.imwrite(f'{folder}/input/{i+1}.jpg', img)
+
+def extract1(folder, data):
+	os.mkdir(f'{folder}/output')
+	data = np.swapaxes(data, 2, 0)
+
+	for i,img in enumerate(data):
+		cv2.imwrite(f'{folder}/output/{i+1}.jpg', img)
 
 
 
--- to_PDF.py
+++ to_PDF.py
@@ -1,12 +1,95 @@
-from latexdocs import Document, TikZFigure, Image
-from pylatex import Alignat, Matrix, Math, Tabular, Plot
-from pylatex.utils import italic
+from pylatex import Document, Figure, SubFigure, Section, PageStyle, Head, Foot, MiniPage, \
+	LargeText, MediumText, LineBreak, StandAloneGraphic, simple_page_number, TextColor, Command, NewLine, Tabu, Center, \
+	TextBlock, VerticalSpace, NewPage
+from pylatex.utils import italic, bold, NoEscape
 import numpy as np
 
-def generate_PDF(folder):
-	doc = Document()
 
-	img = Image(filename='../../images/logo.png', position='h!', width='100px')
-	doc.append(img)
+def generate_PDF(folder, in_time, out_time, num_files):
+	doc = Document(geometry_options={'left': '0.625in', 'right': '0.625in', 'top': '0.75in', 'bottom': '0.75in', 'includeheadfoot': True})
 
-	doc.build().generate_pdf(f'{folder}/report', compiler='pdflatex')
+	# Generating first page style
+	first_page = PageStyle('firstpage')
+
+	# Header image
+	with first_page.create(Head('L')) as header_left:
+		with header_left.create(MiniPage(width=NoEscape(r'0.49\textwidth'), pos='c')) as logo_wrapper:
+			logo_wrapper.append(StandAloneGraphic(image_options='width=100px', filename='../../images/logo.png'))
+			# logo_wrapper.append(LineBreak())
+			# logo_wrapper.append(LineBreak())
+			# logo_wrapper.append(StandAloneGraphic(image_options=r'width=525px', filename='images/banner.jpg'))
+
+	# Header image
+	with first_page.create(Head('R')) as header_right:
+		with header_right.create(MiniPage(width=NoEscape(r'0.49\textwidth'), pos='c', align='r')) as title_wrapper:
+			title_wrapper.append(simple_page_number())
+
+	# Footer
+	with first_page.create(Foot('C')) as footer:
+		with footer.create(MiniPage(width=NoEscape(r'\textwidth'), pos='c', align='c')) as footer_wrapper:
+			footer_wrapper.append(TextColor('gray', italic('Note that this stroke diagnosis report is for demo only, the results cannot be used in any clinics.')))
+
+	doc.preamble.append(first_page)
+
+	# Title
+	with doc.create(Center()):
+		doc.append(VerticalSpace("2mm"))
+		doc.append(Command('fontsize', arguments=['20', '0']))
+		doc.append(Command('selectfont'))
+		doc.append(bold('Stroke Diagnosis Report'))
+		doc.append(VerticalSpace("5mm"))
+
+	# Patient info
+	with doc.create(Tabu('X[l] X[2l] X[l] X[2l]', row_height=2)) as info:
+		info.add_hline()
+		info.add_empty_row()
+		info.add_row(['Patient Name:', 'Sample Patient', 'Modality:', 'Computed Tomography (CT)'])
+		info.add_row(['Date of Birth:', 'DD-MM-YYYY', 'Registered on:', in_time])
+		info.add_row(['Gender:', 'Unknown', 'Reported on:', out_time])
+		info.add_empty_row()
+		info.add_hline()
+
+	# Diagnosis tile and content
+	with doc.create(Tabu('X[l]', row_height=2)) as diagnosis_title:
+		diagnosis_title.add_empty_row()
+		doc.append(Command('fontsize', arguments=['12', '0']))
+		doc.append(Command('selectfont'))
+		diagnosis_title.add_row([NoEscape(r'\underline{Diagnosis Report:}')], mapper=bold)
+
+	with doc.create(Tabu('X[l]', row_height=2)) as diagnosis:
+		diagnosis.add_row(['Each diagnosis is dependent on the symptoms and complexity of the condition that might be causing them, so there is no set timeline as to when you will know your results. It\'s important to stay in contact with your healthcare provider during your diagnostic process especially if you have any changes to your symptoms or medical history, which could affect the results of your diagnosis.'])
+		diagnosis.add_empty_row()
+		diagnosis.add_hline()
+
+	# Lesion segmentation title
+	with doc.create(Tabu('X[l]', row_height=2)) as title:
+		title.add_empty_row()
+		doc.append(Command('fontsize', arguments=['12', '0']))
+		doc.append(Command('selectfont'))
+		title.add_row([NoEscape(r'\underline{Lesion Segmentation:}')], mapper=bold)
+
+	doc.change_document_style('firstpage')
+
+	for i in range(1, num_files+1):
+		if i % 2 == 1:
+			# Begin second page, segmentation results
+			doc.append(NewPage())
+
+			# Title
+			with doc.create(Center()):
+				doc.append(Command('fontsize', arguments=['20', '0']))
+				doc.append(Command('selectfont'))
+				doc.append(bold('Stroke Diagnosis Report'))
+				doc.append(VerticalSpace("5mm"))
+		
+		# Figure: input CT & segmentation
+		with doc.create(Figure(position='h!')) as fig:
+			doc.append(Command('centering'))
+			with doc.create(SubFigure(position='b', width=NoEscape(r'0.48\linewidth'))) as original:
+				original.add_image(f'input/{i}.jpg', width=NoEscape(r'0.95\linewidth'))
+			with doc.create(SubFigure(position='b', width=NoEscape(r'0.48\linewidth'))) as segmentation:
+				segmentation.add_image(f'output/{i}.jpg', width=NoEscape(r'0.95\linewidth'))
+			fig.add_caption('Original CT scan (left) and stroke lesion segmentation (right).')
+
+
+	doc.generate_pdf(f'{folder}/report', compiler='latexmk', clean=True, clean_tex=False, compiler_args=['-pdf'])
